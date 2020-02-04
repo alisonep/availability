3af1505b10e9ee0987af3e3a0b33a5d315d8545a
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-02-03 23:21:03 -0500

    data is retrieved from database and set in app state

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      breedId: 1,
+      dogs: []
+    }
+  }
+
+  dogFinder() {
+    axios.get('/getAllMatchingBreed', {
+      params: {
+        key: this.state.breedId
+      }
+    })
+    .then((res) => {
+      console.log('Response from dogFinder: ', res);
+      this.setState({
+        dogs: res.data
+      })
+    })
+    .catch((err) => {
+      if (err) { throw err; }
+    })
+  }
+  
+  componentDidMount() {
+    this.dogFinder();
   }
 
   render() {
     return (
       <div>
-        <h1>Availability</h1>
+        <h1>{this.state.breedId}s Availabile Nearby</h1>
+        <h3>{Math.max(this.state.dogs.length - 4, 0)} more {this.state.breedId}s available for adoption</h3>
       </div>
     )
   }
--- database/seed.js
+++ database/seed.js
@@ -17,4 +17,4 @@ async function connectDatabase() {
   seedSampleDogs();
 }
 
-connectDatabase()
+connectDatabase();
